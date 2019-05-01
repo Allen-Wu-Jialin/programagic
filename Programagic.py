9ba49605f2ac0cd7f0a4c4b30564620fcd477d12
Allen Wu <49053242+Allen-Wu-Jialin@users.noreply.github.com>
2019-05-01 13:29:23 -0400

    Add files via upload

---
+++ Programagic.py
@@ -0,0 +1,213 @@
+import random, sys, time, math, pygame
+from physics import *
+from pygame.locals import *
+
+global fps, display_width, display_length, screen_one, screen_two,screen_three,screen_four
+#initiation and vars
+pygame.init()
+fps = 60
+display_width = 1500
+display_length = 1071
+screen_one = False #game
+screen_two = True #level
+screen_three = False #setting
+screen_four = False #menu
+screen_five = False #opening screen
+#colors:
+Aqua = (0,255,255)
+Black = (0,0,0)
+Blue = (0,0,255)
+Fuchsia = (255,0,255)
+Gray = (128,128,128)
+Green = (0,128,0)
+Lime = (0,255,0)
+Maroon = (128,0,0)
+Navy_Blue = (0,0,128)
+Olive = (128,128,0)
+Purple = (128,0,128)
+Red = (255,0,0)
+Silver = (192,192,192)
+Teal = (0,128,128)
+White = (255,255,255)
+Yellow = (255,255,0)
+#create display and name of it and a clock
+gameDisplay = pygame.display.set_mode((display_width,display_length))
+pygame.display.set_caption('Graphic Test')
+fpsClock = pygame.time.Clock()
+pygame.font.init() 
+levelFont = pygame.font.SysFont('Comic Sans MS', 60)
+textsurface = levelFont.render('1', False, Black)
+#loads all the images for the screens in
+setting_screen = pygame.image.load('Settings.jpg')
+level_screen = pygame.image.load('map.jpg')
+menu_screen = pygame.image.load('Home_screen.jpg')
+#game_screen = pygame.image.load('')#not created yet
+#opening_screen = pygame.image.load('') #not created yet
+
+#methods to display the screens
+
+def level_Screen():
+    gameDisplay.blit(level_screen, (0,0))
+def menu_Screen():
+    gameDisplay.blit(menu_screen, (0,0))
+def setting_Screen():
+    gameDisplay.blit(setting_screen, (0,0))
+def game_Screen():
+    gameDisplay.blit(game_screen, (0,0))
+def opening_Screen():
+    gameDisplay.blit(opening_screen, (0,0))
+#method for opening screen clicks
+def onClickScreen():
+    for event in pygame.event.get():
+        if event.type:
+            screen_five = False
+            screen_four = True
+            
+#method to run the game
+def run_Game():
+    #position var for the object
+    x = (display_width*0.02)
+    y = (display_length*0.65)
+
+    x_change = 0
+    y_change = 0
+    someObj = thing([0,0], 1)
+    for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_Exit = True
+            #movement control
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT and not(x < 0 + characterIMG_width/2):
+                    x_change = -5
+                elif event.key == pygame.K_RIGHT and not(x> display_width - characterIMG_width):
+                    x_change = 5
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    x_change = 0
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP and not(y < 0):
+                    y_change = -5
+                elif event.key == pygame.K_DOWN and not(y> (display_length - characterIMG_length)):
+                    y_change = 5
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                    y_change = 0
+            x += x_change
+            y += y_change
+            someObj.thrust(x,y)
+#button methods
+def menu_Buttons():
+    x_pos =
+    y_pos =
+    obj_len =
+    obj_width =
+    currentScreen = 'menu'
+    settingScreen = 'settings'
+    mapScreen = 'map'
+    button(x_pos, y_pos, obj_len, obj_width, currentScreen, )
+    
+def map_Buttons():
+    x_pos =
+    y_pos =
+    obj_len =
+    obj_width =
+    currentScreen = 'map'
+    settingScreen = 'settings'
+    gameScreen = 'game'
+    menuScreen = 'menu'
+    button(x_pos, y_pos, obj_len, obj_width, currentScreen, )
+ 
+def setting_Buttons():
+    x_pos =
+    y_pos =
+    obj_len =
+    obj_width = 
+    currentScreen = 'settings'
+    menuScreen = 'menu'
+    button(x_pos, y_pos, obj_len, obj_width, currentScreen, )
+#cS current Screen. oS other Screen
+#generates the button
+def button(x,y,lenth,width,cS, oS):
+    mouse = pygame.mouse.get_pos()
+    #print(mouse)
+    click = pygame.mouse.get_pressed()
+    #otherClick = pygame.MOUSEBUTTONUP
+    #print(click)
+    if x+length > mouse[0] >x and y+width > mouse [1] > y:
+        if click[0] == 1:
+            if(cS == 'setting' and oS == 'menu'):
+                screen_four = True
+                screen_three = False
+            elif(cS == 'map' and oS == 'menu'):
+                screen_four = True
+                screen_two = False
+            elif(cS == 'map' and oS == 'game'):
+                screen_one = True
+                screen_two = False
+            elif(cS == 'map' and oS == 'settings'):
+                screen_three = True
+                screen_two = False
+            elif(cS == 'map' and oS == 'menu'):
+                screen_four = True
+                screen_two = False
+                
+            
+    else:
+        pass
+        
+
+def game_loop():
+    #global fps, display_width, display_length, screen_one, screen_two,screen_three,screen_four
+
+
+
+    game_Exit = False
+    #game update thing
+    while not game_Exit:
+        
+
+        '''if event.type == QUIT:
+            pygame.quit() 
+'''
+
+        if(screen_one):
+            game_Screen()
+            run_Game()
+        elif(screen_two):
+            level_Screen()
+            map_Buttons()
+        elif(screen_three):
+            setting_Screen()
+            setting_Buttons()
+        elif(screen_four):
+            menu_Screen()
+            menu_Buttons()
+        elif(screen_five):
+            opening_Screen()
+            onClickScreen()
+            
+        else:
+            pass
+            
+        #if x> display_width - characterIMG_width or x < 0:
+            #out_of_bounds()
+        #if y> display_length - characterIMG_length or x < 0:
+            #out_of_bounds()
+        #print (event)
+        #mouse = pygame.mouse.get_pos()
+        #print(mouse)
+        #if 164+76+76 > mouse[0] >164 and 247+76+76 > mouse [1] > 247:
+            #pygame.draw.circle(gameDisplay, green, (164,247), 76)
+        #else:
+            #pygame.draw.circle(gameDisplay, yellow, (164,247), 76)
+        pygame.display.update()
+        
+        fpsClock.tick(fps)
+
+
+
+
+
+game_loop() 
+pygame.quit()
+quit()
---
+++ physicsRender.py
@@ -0,0 +1,197 @@
+﻿input("")
+things = []
+
+class thing():
+	def __init__(self, loc, mass = 1, xrad = 1, yrad = 1):
+		global things
+		things += [self]
+		
+		self.location = loc
+		self.gravity = [0, -0.5]
+		self.__velocity = [0, 0]
+		self.mass = mass
+		self.xrad = xrad
+		self.yrad = yrad
+		self.immobile = False
+		self.collidable = True
+		self.drag = 0.9
+		self.token = "@"
+
+	def thrust(self, vect, two = ''):
+		try:
+			self.__velocity[0] += vect[0]
+			self.__velocity[1] += vect[1]
+		except: # Just being nice and making it so you don't *need* an array. thrust(1,2) and thrust([1,2]) will do the same thing.
+			self.__velocity[0] += vect
+			self.__velocity[1] += two
+
+	def getVelocity(self):
+		return self.__velocity
+
+	def __move(self, vect): # Called when ticking to trace collision.
+		if not self.collidable: # Things that don't collide don't need tracing.
+			self.location[0] += vect[0]
+			self.location[1] += vect[1]
+			return True
+
+		if self.collision(self.location) != False: # Hopefully prevent clipping crashes
+			print("Clipping error!")
+			self.location = [self.location[0] - self.__velocity[0], self.location[1] - self.__velocity[1]]
+			self.__velocity = [self.__velocity[0] * -1, self.__velocity[1] * -1]
+			return False
+#			o = self.collision(self.location)
+#			if self.location[0] < o.location[0]:
+#				if self.__velocity[0] > 0:
+#					self.__velocity[0] += 1
+#					self.__velocity[0] = self.__velocity[0] * -1
+#				else:
+#					self.__velocity[0] -= 1
+#			elif self.location[0] > o.location[0]:
+#				if self.__velocity[0] < 0:
+#					self.__velocity[0] -= 1
+#					self.__velocity[0] = self.__velocity[0] * -1
+#				else:
+#					self.__velocity[0] += 1
+#			if self.location[1] < o.location[1]:
+#				if self.__velocity[1] > 0:
+#					self.__velocity[1] += 1
+#					self.__velocity[1] = self.__velocity[1] * -1
+#				else:
+#					self.__velocity[1] -= 1
+#			elif self.location[1] > o.location[1]:
+#				if self.__velocity[1] < 0:
+#					self.__velocity[1] -= 1
+#					self.__velocity[1] = self.__velocity[1] * -1
+#				else:
+#					self.__velocity[1] += 1
+#			return
+			
+		if vect[0] > self.xrad or vect[1] > self.yrad: # If it's moving more than its own size in the tick, trace collision recursively.
+			nvec = [vect[0] * 0.5, vect[1] * 0.5]
+			if self.__move(nvec):
+				if self.__move(nvec):
+					return True
+				return False
+			return False
+		
+		nloc = [self.location[0] + vect[0], self.location[1] + vect[1]] # Now we actually trace for collision
+		if self.collision(nloc) == False: 
+			self.location = nloc
+			return True
+		o = self.collision(nloc) # Now that we know what we're colliding with, it's time to find where we hit.
+		minx = nloc[0] - (self.xrad/2)
+		maxx = nloc[0] + (self.xrad/2)
+		miny = nloc[1] - (self.yrad/2)
+		maxy = nloc[1] + (self.yrad/2)
+		ominx = o.location[0] - (o.xrad/2)
+		omaxx = o.location[0] + (o.xrad/2)
+		ominy = o.location[1] - (o.yrad/2)
+		omaxy = o.location[1] + (o.yrad/2)
+		
+		if ominx < maxx and omaxx > minx: # Overlapping on x
+			if self.location[0] < o.location[0]:
+				nx = o.location[0] - (o.xrad/2+self.xrad/2) # If self is to the left, stop short on the left
+			else:
+				nx = o.location[0] + (o.xrad/2+self.xrad/2)
+		else:
+			nx = nloc[0] # If not overlapping, don't stop short
+		
+		if ominy < maxy and omaxy > miny: # As above, but y
+			if self.location[1] < o.location[1]:
+				ny = o.location[1] - (o.yrad/2+self.yrad/2)
+			else:
+				ny = o.location[1] + (o.yrad/2+self.yrad/2)
+		else:
+			ny = nloc[1]
+		
+		mm = 1 # Mass ratio as a modifier for the collision
+		#self.location = [nx, ny]
+
+		# Let's find a rough vector of collision to bounce from
+		if (minx > ominx and maxx < omaxx) or (ominx > minx and omaxx < maxx): # Parallel along x
+			nx = 0
+		else:
+			nx = 1
+		
+		if (miny > ominy and maxy < omaxy) or (ominy > miny and omaxy < maxy): # Parallel along y
+			ny = 0
+		else:
+			ny = 1
+		
+		if not o.immobile:
+			o.thrust(self.__velocity[0] * mm * nx * 0.5, self.__velocity[1] * mm * ny * 0.5)
+		self.thrust(self.__velocity[0] / mm * nx * -0.5, self.__velocity[1] / mm * ny * -0.5)
+		return False
+
+	def tickMe(self):
+		if self.immobile:
+			self.__velocity = [0,0]
+			return
+		self.thrust(self.gravity)
+		self.__move(self.__velocity)
+		self.__velocity = [self.__velocity[0] * self.drag, self.__velocity[1] * self.drag]
+
+	def collision(self, loc): # Checks if there would be something collidable overlapping with the object if it were at "loc".
+		global things
+		
+		minx = loc[0] - (self.xrad/2)
+		maxx = loc[0] + (self.xrad/2)
+		miny = loc[1] - (self.xrad/2)
+		maxy = loc[1] + (self.xrad/2)
+
+		for o in things:
+			if o != self:
+				ominx = o.location[0] - (o.xrad/2)
+				omaxx = o.location[0] + (o.xrad/2)
+				ominy = o.location[1] - (o.yrad/2)
+				omaxy = o.location[1] + (o.yrad/2)
+
+				if ominx < maxx and omaxx > minx and ominy < maxy and omaxy > miny and o.collidable:
+					return o
+		return False
+
+ticker = thing([0,0])
+ticker.collidable = False
+
+def tick():
+	global things
+	global ticker
+	import math
+	for i in things:
+		i.tickMe()
+		#i.location = [i.location[0] % 80, i.location[1] % 40]
+	s = ""
+	for q in range(40):
+		for j in range(80):
+			if ticker.collision([j, q]) == False:
+				s += " "
+			else:
+				s += ticker.collision([j, q]).token
+		if q != 39:
+			s += "\n"
+	print(s)
+
+import time
+time.sleep(2)
+
+a = thing([5,35], 1, 1, 1)
+a.gravity = [0, 0.1]
+a.thrust(5, -5)
+ground = thing([39,39], 16, 80, 1)
+ground.immobile = True
+ground.token = "█"
+ceil = thing([39,0], 16, 80, 1)
+ceil.immobile = True
+ceil.token = "█"
+rwall = thing([79,19], 16, 1, 40)
+rwall.immobile = True
+rwall.token = "█"
+lwall = thing([0,19], 16, 1, 40)
+lwall.immobile = True
+lwall.token = "█"
+b = thing([10, 20], 4, 2, 2)
+b.gravity = [0.1, -0.1]
+b.drag = 1
+b.token = "!"
+while True:
+        tick()
